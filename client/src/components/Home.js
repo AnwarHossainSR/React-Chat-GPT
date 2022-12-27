0bd58a1cf59bed76a46349fbd960225205dec299
Anwar Hossain <anwar.sarker@bjitgroup.com>
2022-12-27 17:07:31 +0600

    update on novu chat

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,49 +1,34 @@
-import React, { useState } from 'react';
+import {
+  NotificationBell,
+  NovuProvider,
+  PopoverNotificationCenter,
+} from '@novu/notification-center';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
-  const [message, setMessage] = useState('');
-  const [subscriber, setSubscriber] = useState('');
+  const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log({ message, subscriber });
-    setMessage('');
-    setSubscriber('');
+  const onNotificationClick = (notification) => {
+    navigate(notification.cta.data.url);
   };
+  //...other statements
 
   return (
     <div className='home'>
       <nav className='navbar'>
         <h2>Notify</h2>
-      </nav>
-      <main className='homeContainer'>
-        <h3>Send notifications to your users</h3>
-        <form
-          className='notification__form'
-          onSubmit={handleSubmit}
-          method='POST'
+        <NovuProvider
+          subscriberId={'<YOUR_SUBSCRIBER_ID>'}
+          applicationIdentifier={'<YOUR_APP_ID>'}
         >
-          <label htmlFor='title'>Notification Title</label>
-          <textarea
-            rows={5}
-            name='title'
-            required
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            placeholder='Let the user know that'
-          />
-          <label htmlFor='subscriber'>Subscribers</label>
-
-          <select
-            value={subscriber}
-            name='subscriber'
-            onChange={(e) => setSubscriber(e.target.value)}
-          >
-            <option value='Select'>Select</option>
-          </select>
-          <button>SEND NOTIFICATION</button>
-        </form>
-      </main>
+          <PopoverNotificationCenter onNotificationClick={onNotificationClick}>
+            {({ unseenCount }) => (
+              <NotificationBell unseenCount={unseenCount} colorScheme='light' />
+            )}
+          </PopoverNotificationCenter>
+        </NovuProvider>
+      </nav>
+      <main className='homeContainer'>...</main>
     </div>
   );
 };
